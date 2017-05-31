eb6d82981d88aa940cba6e2934c44690c8744442
momohatt <momoko.hattori.1006@gmail.com>
2017-05-31 23:21:01 +0900

    improved it
    see_if_included関数を使うことでsortの必要がなくなった気がするので，sortなしのバージョンも作ります。

--- anagram.cpp
+++ anagram.cpp
@@ -4,17 +4,19 @@
 #include<fstream> //ifstream, ofstream
 using namespace std;
 
+bool see_if_included(string, string);
+int score(string);
+
 int n=72412;
 int m=16; //16
 string str[16];//input //16
 string str_sorted;
-string dict[72412];//72412
+string dict[72412];//72412(the number of words in the dict)
 string dict_sorted[72412];//72412
 
 int main(){
 	
 	//input&sort
-
 	for(int i=0;i<m;++i){
 		cin>>str[i];
 	}
@@ -33,42 +35,16 @@ int main(){
 		exit(0);
 	}
 
-	/* //I think I made a mistake here so decided to discard it
-	while(!reading_file.eof()){
-		for(int i=0;i<n;++i){
-			getline(reading_file, dict[i]);
-			int length=dict[i].length(); //length of the word
-			//cout<<dict[i]<<" "<<dict[i].length()<<endl;
-			if(length>16) continue;
-			string letters[16]; //ex) apple -> a p p l e
-
-			for(int j=0;j<length;++j){
-				letters[j]=dict[i].substr(j,1);
-				//cout<<letters[j]<<" ";
-			}
-			//cout<<endl;
-
-			sort(letters, letters+length);
-
-			for(int j=0;j<length;++j){
-				dict_sorted[i]+=letters[j];
-			}
-
-			cout<<"End of the while sentence"<<endl;
-			cout<<dict_sorted[i]<<endl;
-		}
-	}*/
-
 	int i=0;
 	while(!reading_file.eof()){
 		getline(reading_file, dict[i]);
 		int length=dict[i].length();
-		if(length>m){
-			//cout<<dict[i]<<endl;
-			continue;
-		}
+
+		if(length>m) continue;
+
 		string letters[16];
 
+		transform(dict[i].begin(), dict[i].end(), dict[i].begin(), ::tolower);
 		for(int j=0;j<length;++j){
 			letters[j]=dict[i].substr(j,1);
 		}
@@ -81,20 +57,82 @@ int main(){
 
 		//cout<<"the end"<<endl;
 		//cout<<i+1<<" "<<dict_sorted[i]<<" "<<dict[i]<<endl;
-
 		++i;
 	}
 
-	//try 2^16 cases
-
 	//search phase&output
-	for(int k=0;k<n;++k){
+
+	int highest_score=0;
+	int memory; //indicate which word scores most
+	for(int i=0;i<n;++i){ //for all the words in dict
+
 		//cout<<"In the searching phase"<<endl;
-		//cout<<dict_sorted[k]<<endl;
-		if(dict_sorted[k]==str_sorted){
-			cout<<"Answer: "<<dict[k]<<endl;
+		//cout<<dict_sorted[i]<<endl;
+		if(dict_sorted[i].empty()) continue;
+
+		if(see_if_included(dict_sorted[i], str_sorted)){
+			if(score(dict_sorted[i])>highest_score){
+				highest_score=score(dict_sorted[i]);
+				memory=i;
+			}
 		}
 	}
 
+	cout<<"Answer: "<<dict[memory]<<endl;
+
 	return 0;
+}
+
+int score(string word)
+{
+   	string letter[26]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    	int point[26]={1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3};
+    	int score=0;
+	int l=word.length();
+
+    	for(int i=0;i<l;++i){
+        	for(int j=0;j<26;++j){
+            	if(word.substr(i,1)==letter[j]){
+                	score+=point[j];
+                	break;
+            		}
+        	}
+    	}
+    
+    	++score;
+    	score=score*score;
+    	return score;
+}
+
+
+bool see_if_included(string word, string str)
+{
+    	int l=word.length();
+    	int counter=0;
+    	string str_prsv=str;//preserve
+        
+    	for(int i=0;i<l;++i){ //for all the letters in the word
+        //int num=0;//=0(if no matching) or =1(matched)
+            
+        	for(int j=0;j<m;++j){ //for all the letters in the str
+            		if(word.substr(i,1)==str.substr(j,1)){
+                		str.replace(j,1, "0"); //replace it with non-alphabet
+                		//cout<<"current str: "<<str<<endl;
+                		//num=1;
+                		//cout<<num<<endl;
+                		++counter;
+                		//cout<<str<<" "<<str_prsv.substr(j,1)<<" "<<counter<<endl;
+                		break;
+            		}
+        	}	
+    	}
+        
+    	//str=str_prsv; //to restore not necessary?
+        
+    	if(counter==l){
+        	return true;
+    	} else {
+        	return false;
+    	}
+        
 }
