b0b5e64c704a1832b7cb2eafb57bec46d457fbfa
momohatt <momoko.hattori.1006@gmail.com>
2017-06-07 14:46:09 +0900

    completed

--- week2/hw1.cpp
+++ week2/hw1.cpp
@@ -1,6 +1,55 @@
 #include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int time();
 
 int main (void)
 {
+    int n;
+    cout<<"配列の大きさを入力：";
+    cin>>n;
+    vector< vector<int> > a,b,c;
+    a=b=c=vector< vector<int> >(n, vector<int>(n));
+
+    //input
+    cout<<"配列Aの要素を入力："<<endl;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cin>>a[i][j];
+        }
+    }
+
+    cout<<"配列Bの要素を入力："<<endl;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cin>>b[i][j];
+        }
+    }
+
+    //計算
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            c[i][j]=0;
+            for(int k=0;k<n;++k){
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    //output
+    cout<<"配列AB="<<endl;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<n;++j){
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"計算時間"<<endl;
+
     return 0;
+
 }
+
